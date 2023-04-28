e4b4f4dc92b29cefef4df1658c3f23f8de1ba550
Kristian Karlson <107461637+Bremmster@users.noreply.github.com>
2023-04-28 13:11:19 +0200

    added class person

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+/**
+ * @author Kristian Karlson
+ */
+public class Person {
+
+    // Skapa en Java-modell för "Person" med följande egenskaper: "namn", "ålder" och "adress".
+
+    private String name;
+    private int age;
+    private String address;
+
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+}
