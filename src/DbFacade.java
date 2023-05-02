6fb5197a38329b20bcda49834493daa0bd7d9567
Kristian Karlson <107461637+Bremmster@users.noreply.github.com>
2023-05-02 22:05:51 +0200

    improved connect

--- src/DbFacade.java
+++ src/DbFacade.java
@@ -2,21 +2,35 @@
  * @author Kristian Karlson
  */
 
-import com.mongodb.*;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import org.bson.BsonDocument;
-import org.bson.BsonInt64;
 import org.bson.Document;
-import org.bson.conversions.Bson;
 
 
 public class DbFacade {
-    public static void connect(KeyReader key) {
+
+    String collectionName;
+    MongoClient mongoClient;
+    private MongoDatabase db;
+    private MongoCollection<Document> collection;
+    private String connString;
+    private String dbName;
+    public DbFacade(String connString, String dbName, String collectionName) {
+        this.connString = connString;
+        this.dbName = dbName;
+        this.collectionName = collectionName;
+    }
+
+    public void connect() {
 
         // Replace the placeholder with your Atlas connection string
-        String uri = "mongodb+srv://" + key.getKey("usrName") + ":" + key.getKey("apiKey") + "@cluster0.lb6kqnd.mongodb.net/?retryWrites=true&w=majority";
+        // String uri = "mongodb+srv://" + key.getKey("usrName") + ":" + key.getKey("apiKey") + "@cluster0.lb6kqnd.mongodb.net/?retryWrites=true&w=majority";
 
         // Construct a ServerApi instance using the ServerApi.builder() method
         ServerApi serverApi = ServerApi.builder()
@@ -24,25 +38,25 @@ public class DbFacade {
                 .build();
 
         MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(new ConnectionString(uri))
+                .applyConnectionString(new ConnectionString(connString))
                 .serverApi(serverApi)
                 .build();
 
-        // Create a new client and connect to the server
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
-            MongoDatabase database = mongoClient.getDatabase("Person");
-            try {
-                // Send a ping to confirm a successful connection
-                Bson command = new BsonDocument("ping", new BsonInt64(1));
-                Document commandResult = database.runCommand(command);
-                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-            } catch (MongoException me) {
-                System.err.println(me);
-            }
-        }
+
+        this.mongoClient = MongoClients.create(settings);
+        this.db = mongoClient.getDatabase(this.dbName);
+        System.out.println("You successfully connected to MongoDB!");
+        this.collection = db.getCollection(collectionName);
     }
 
+
     public void insertOne() {
-        
+
+    }
+
+    public void close() {
+        if (mongoClient != null) {
+            mongoClient.close();
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println(keyReader.getKey("usrName"));
 
 //        DbFacade db = new DbFacade();
-        DbFacade.connect(keyReader);
+
 
 
 
